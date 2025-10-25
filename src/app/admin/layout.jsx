22b9763a61b6a6cf85327f68f93de4827de61407
geugyeonglabs <geugyeonglabs@gmail.com>
2025-10-25 20:53:59 +0900

    feat: 해당 날짜의 미팅 리스트 렌더링

--- src/app/admin/layout.jsx
+++ src/app/admin/layout.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Form from "next/form";
 import {
   Button,
@@ -17,48 +17,16 @@ import {
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import dayjs from "dayjs";
-import {
-  CalendarIcon,
-  ChartPieIcon,
-  DocumentDuplicateIcon,
-  FolderIcon,
-  HomeIcon,
-  UsersIcon,
-} from "@heroicons/react/24/outline";
+import { UsersIcon } from "@heroicons/react/24/outline";
 
 import { supabase } from "@/utils/supabaseClient";
-
-const navigation = [
-  { name: "Dashboard", href: "#", icon: HomeIcon, count: "5", current: true },
-  { name: "Team", href: "#", icon: UsersIcon, current: false },
-  {
-    name: "Projects",
-    href: "#",
-    icon: FolderIcon,
-    count: "12",
-    current: false,
-  },
-  {
-    name: "Calendar",
-    href: "#",
-    icon: CalendarIcon,
-    count: "20+",
-    current: false,
-  },
-  { name: "Documents", href: "#", icon: DocumentDuplicateIcon, current: false },
-  { name: "Reports", href: "#", icon: ChartPieIcon, current: false },
-];
-const teams = [
-  { id: 1, name: "Heroicons", href: "#", initial: "H", current: false },
-  { id: 2, name: "Tailwind Labs", href: "#", initial: "T", current: false },
-  { id: 3, name: "Workcation", href: "#", initial: "W", current: false },
-];
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 export default function AdminLayout({ children }) {
+  const [navigation, setNavagation] = useState([]);
   const [open, setOpen] = useState(false);
 
   const [date, setDate] = useState(dayjs());
@@ -80,10 +48,30 @@ export default function AdminLayout({ children }) {
     setOpen(false);
   };
 
-  const handleAddMeeting = async () => {
-    const { error } = await supabase
+  const readMeeting = async () => {
+    const { data, error } = await supabase
       .from("meeting")
-      .insert({ date, name, password, type, matching });
+      .select("*")
+      .eq("date", date);
+
+    data &&
+      setNavagation(
+        data.map((row) => {
+          return { ...row, href: "#", icon: UsersIcon, current: false };
+        })
+      );
+
+    error && console.log(error);
+  };
+
+  const handleCreateMeeting = async () => {
+    const { error } = await supabase.from("meeting").insert({
+      date,
+      name: name.trim(),
+      password: password.trim(),
+      type,
+      matching,
+    });
 
     if (error && error.code === "23505") {
       alert("해당 날짜에 같은 이름의 모임이 존재합니다.");
@@ -91,8 +79,30 @@ export default function AdminLayout({ children }) {
       return;
     }
 
+    readMeeting();
+
     handleClose();
   };
+
+  useEffect(() => {
+    const handleReadMeeting = async () => {
+      const { data, error } = await supabase
+        .from("meeting")
+        .select("*")
+        .eq("date", date);
+
+      data &&
+        setNavagation(
+          data.map((row) => {
+            return { ...row, href: "#", icon: UsersIcon, current: false };
+          })
+        );
+
+      error && console.log(error);
+    };
+
+    handleReadMeeting();
+  }, [date]);
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -108,7 +118,10 @@ export default function AdminLayout({ children }) {
           <nav className="relative flex flex-1 flex-col">
             <ul role="list" className="flex flex-1 flex-col gap-y-7">
               <li>
-                <ul role="list" className="-mx-2 space-y-1">
+                <div className="text-xs/6 font-semibold text-gray-400">
+                  미팅
+                </div>
+                <ul role="list" className="-mx-2 mt-2 space-y-1">
                   {navigation.map((item) => (
                     <li key={item.name}>
                       <a
@@ -143,38 +156,6 @@ export default function AdminLayout({ children }) {
                   ))}
                 </ul>
               </li>
-              <li>
-                <div className="text-xs/6 font-semibold text-gray-400">
-                  Your teams
-                </div>
-                <ul role="list" className="-mx-2 mt-2 space-y-1">
-                  {teams.map((team) => (
-                    <li key={team.name}>
-                      <a
-                        href={team.href}
-                        className={classNames(
-                          team.current
-                            ? "bg-gray-50 text-indigo-600 dark:bg-white/5 dark:text-white"
-                            : "text-gray-700 hover:bg-gray-50 hover:text-indigo-600 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-white",
-                          "group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold"
-                        )}
-                      >
-                        <span
-                          className={classNames(
-                            team.current
-                              ? "border-indigo-600 text-indigo-600 dark:border-white/10 dark:text-white"
-                              : "border-gray-200 text-gray-400 group-hover:border-indigo-600 group-hover:text-indigo-600 dark:border-white/15 dark:group-hover:border-white/20 dark:group-hover:text-white",
-                            "flex size-6 shrink-0 items-center justify-center rounded-lg border bg-white text-[0.625rem] font-medium dark:bg-white/5"
-                          )}
-                        >
-                          {team.initial}
-                        </span>
-                        <span className="truncate">{team.name}</span>
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </li>
               <li className="-mx-6 mt-auto text-center">
                 <Button variant="contained" onClick={handleClickOpen}>
                   미팅 만들기
@@ -189,9 +170,9 @@ export default function AdminLayout({ children }) {
         <DialogTitle>미팅 만들기</DialogTitle>
         <DialogContent>
           <Form
-            id="add-meeting"
+            id="create-meeting"
             className="flex flex-col gap-4 p-2"
-            action={handleAddMeeting}
+            action={handleCreateMeeting}
           >
             <TextField
               label="모임 이름"
@@ -236,7 +217,7 @@ export default function AdminLayout({ children }) {
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>취소</Button>
-          <Button type="submit" form="add-meeting">
+          <Button type="submit" form="create-meeting">
             저장
           </Button>
         </DialogActions>
